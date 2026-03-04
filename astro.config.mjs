1f6159372e81cfea34acfbb1cbfbd1a6f56ff457
Gaston <gastonhoyo@gmail.com>
2026-03-03 22:28:48 -0300

    un mensaje descriptivo

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
-export default defineConfig({
-  site: 'https://chieffin.com',
-  base: '/Calculadoras', // <-- QUE COINCIDA CON EL NOMBRE DEL REPO NUEVO
-  integrations: [tailwind()],
-});
