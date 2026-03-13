84a2b574222c43d49446a0d5b4abab5fe04ba4c7
Gaston <gastonhoyo@gmail.com>
2026-03-13 11:00:54 -0300

    un mensaje descriptivo

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,8 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   output: 'server',
   adapter: cloudflare({
-    // 'directory' es el modo recomendado para Pages porque separa 
-    // automáticamente lo que es servidor de lo que es estático (CSS/JS)
-    mode: 'directory', 
+    mode: 'directory', // Este modo es el que mejor se lleva con los archivos estáticos
   }),
   integrations: [tailwind()],
-  // Forzamos a que las rutas de los assets sean relativas a la raíz
-  build: {
-    assets: '_astro'
-  }
+  base: '/',
 });
